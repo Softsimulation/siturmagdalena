e575ed51771414c6065bc6fe3d5ce5f3531e3f08
Andres Paternina <eduardoandrespaternina@gmail.com>
2018-09-10 15:52:01 +0000

    Proteccion rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,4 +3,97 @@
 
 
 
+Route::controller('/indicadores','IndicadoresCtrl');
+
+Route::controller('/EstadisticasSecundarias','EstadisticasSecundariasCtrl');
+
+Route::controller('/temporada','TemporadaController');
+Route::controller('/turismointerno','TurismoInternoController');
+
+Route::controller('/turismoreceptor','TurismoReceptorController');
+
+Route::controller('/grupoviaje','GrupoViajeController');
+
+Route::controller('/sostenibilidadhogares','SostenibilidadHogaresController');
+
 Route::controller('/administradoratracciones', 'AdministradorAtraccionController');
+
+Route::controller('/administradoractividades', 'AdministradorActividadesController');
+
+Route::controller('/administradordestinos', 'AdministradorDestinosController');
+
+Route::controller('/administrarmunicipios', 'AdministrarMunicipiosController');
+
+
+
+Route::controller('/sostenibilidadpst', 'SostenibilidadPstController');
+
+Route::controller('/importarRnt','ImportacionRntController');
+
+Route::controller('/administradorproveedores', 'AdministradorProveedoresController');
+
+Route::controller('/administradoreventos', 'AdministradorEventosController');
+
+Route::controller('/administradorrutas', 'AdministradorRutasController');
+
+Route::controller('/administrardepartamentos', 'AdministrarDepartamentosController');
+Route::controller('/ofertaempleo','OfertaEmpleoController');
+
+Route::get('/actividades', 'TurismoReceptorController@actividades');
+
+Route::controller('/administrarpaises', 'AdministrarPaisesController');
+
+Route::get('/CrearGrupoViaje', function () {
+    return view('CrearGrupoViaje');
+});
+
+Route::get('/', function () {
+    return view('home.index');
+});
+
+
+Route::controller('/MuestraMaestra','MuestraMaestraCtrl');
+
+
+Route::get('/encuestaAdHoc/{encuesta}/registro', 'EncuestaDinamicaCtrl@getRegistrodeusuarios' );
+Route::get('/encuestaAdHoc/{encuesta}', 'EncuestaDinamicaCtrl@encuesta' );
+Route::get('/llenarEncuestaAdHoc/{idEncuesta}', 'EncuestaDinamicaCtrl@anonimos' );
+Route::controller('/encuesta','EncuestaDinamicaCtrl');
+
+Route::controller('/informes','InformesCtrl');
+
+
+Route::controller('/bolsaEmpleo','BolsaEmpleoController');
+
+Route::controller('/usuario','UsuarioController');
+Route::controller('/email','EmailController');
+Route::controller('/login','LoginController');
+Route::controller('/noticias','NoticiaController');
+
+Route::group(['prefix' => 'publicaciones','middleware'=>'auth'], function () {
+    
+    Route::get('/listadonuevas', 'PublicacionController@publicaciones');
+    Route::get('/crear', 'PublicacionController@CrearPublicacion');
+    Route::get('/editar/{id}', 'PublicacionController@EditarPublicacion');
+    Route::get('/listado', 'PublicacionController@ListadoPublicaciones');
+    Route::get('/listadoadmin', 'PublicacionController@ListadoPublicacionesAdmin');
+    Route::get('/getPublicacion', 'PublicacionController@getPublicacion');
+    Route::get('/getListadoPublico', 'PublicacionController@getListadoPublico');
+    Route::get('/getListado', 'PublicacionController@getListado');
+    Route::post('/guardarPublicacion', 'PublicacionController@guardarPublicacion' );
+    Route::post('/editPublicacion', 'PublicacionController@editPublicacion' );
+    Route::post('/eliminarPublicacion', 'PublicacionController@eliminarPublicacion' );
+    Route::post('/cambiarEstadoPublicacion', 'PublicacionController@cambiarEstadoPublicacion' );
+    Route::get('/getPublicacionEdit/{id}', 'PublicacionController@getPublicacionEdit');
+    Route::post('/EstadoPublicacion', 'PublicacionController@EstadoPublicacion' );
+    
+});
+
+
+
+Route::group(['middleware' => 'cors'], function(){
+   
+   Route::controller('/turismoreceptoroapi','TurismoReceptorCorsController');
+  
+});
+
