a1218264dcfa01a5616de609f3480ab9c3a6ae6e
Jorge Pineda <aplicacionesj@gmail.com>
2018-09-10 14:11:42 +0000

    Corrección en visualización de mapas en ventanas modales

--- resources/views/proveedoresRnt/importarExcel.blade.php
+++ resources/views/proveedoresRnt/importarExcel.blade.php
@@ -384,7 +384,7 @@
         			        </div>
         				</div>
     				    
-                        <div class="row">
+                        <div class="row" style="display: block;">
                             <ng-map id="mapa" zoom="9" center="[11.24079, -74.19904]" map-type-control="true" map-type-control-options="{position:'BOTTOM_CENTER'}"  > 
                                 <marker ng-if="registro.latitud != undefined && registro.longitud != undefined" draggable="true" on-dragend="getCurrentLocation(registro)" position="[@{{registro.latitud}}, @{{registro.longitud}}]" title="registro.nombre_comercial"></marker>
                                 
@@ -726,7 +726,7 @@
     						<!--</div>-->
           <!--              </div>-->
                         
-                        <div class="row">
+                        <div class="row" style="display: block;">
                             <ng-map id="mapa" zoom="9" center="[11.24079, -74.19904]" map-type-control="true" map-type-control-options="{position:'BOTTOM_CENTER'}"  > 
                                 <marker ng-if="registro.latitud != undefined && registro.longitud != undefined" draggable="true" on-dragend="getCurrentLocation(registro)" position="[@{{registro.latitud}}, @{{registro.longitud}}]" title="registro.nombre_comercial"></marker>
                                 
@@ -925,6 +925,7 @@
     <div class='carga'>
 
     </div>
+   
 
 @endsection
 
@@ -984,7 +985,5 @@
         });
 
     </script>
-    <script src="/js/plugins/tokml.js"></script>
     <script src="https://maps.google.com/maps/api/js?libraries=placeses,visualization,drawing,geometry,places"></script>
-    <script src="/js/plugins/ng-map.js"></script>
 @endsection
