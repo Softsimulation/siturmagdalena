9b359d55117cf6298ffc2ce3451ef45663b39f4f
Jorge Pineda <aplicacionesj@gmail.com>
2018-12-05 21:10:34 +0000

    Se incluyó el clima del magdalena en el Magdalena

--- resources/lang/en/resources.php
+++ resources/lang/en/resources.php
@@ -2,10 +2,69 @@
 
 return [
     
-    
-    
-    
-    
+    "header" => [
+        "cargando" => "Loading",
+        "porFavorEspere" => "Please wait",
+        "irAlContenido" => "Go to content",
+        "irA" => "Go to :destino",
+        "mapaDelSitio" => "Site map",
+        "campoDeBusqueda" => "Search field",
+        "queDeseaBuscar" => "¿What do you want to search?",
+        "buscar" => "Search",
+        "seleccionDeIdioma" => "Language select",
+        "espanol" => "Spanish",
+        "ingles" => "English",
+        "cerrarSession" => "Sign out",
+        "iniciarSesion" => "Sign in",
+        "menuDeNavegacion" => "Navigation menu"
+    ],
+    "menu"  => [
+        "inicio" => "Home",
+        "conoceElMagdalena"    => "Visit to Magdalena",
+        "acercaDe"  => "About the department",
+        "requisitosDeViaje"   => "Travel requirements",
+        "planificaTuViaje"   => "Plan your trip",
+        "experiencias" => "Experience",
+        "menuExperiencias" => [
+            "solYPlaya" => "Sun and beach",
+            "cultura" => "Culture",
+            "naturaleza" => "Nature",
+            "nautico" => "Nautical",
+            "religioso" => "Religious",
+            "reunionesYEventos" => "Meetings and events"
+        ],
+        "queHacer" => "What to do",
+        "menuQueHacer" => [
+            "eventos" => "Events",
+            "atracciones" => "Atractions",
+            "actividades" => "Activities",
+            "destinos" => "Destinations",
+            "rutasTuristicas" => "Tourist routes"
+        ],
+        "servicios" => "Services",
+        "menuServicios" => [
+            "alojamientos" => "Accommodations",
+            "establecimientosDeGastronomia" => "Gastronomy establishments",
+            "agenciasDeViaje" => "Travel agencies",
+            "establecimientosDeEsparcimiento" => "Recreation establishments and similar",
+            "transporteEspecializado" => "Specialized transport"
+        ],
+        "publicaciones" => "Publications",
+        "menuPublicaciones" => [
+            "noticias" => "News",
+            "informes" => "Reports",
+            "bibliotecaDigital" => "Digital library"
+        ]
+    ],
+    "estadisticas" => [
+        "turismoReceptor" => "Inbound tourism",
+        "turimoEmisor" => "Outbound tourism",
+        "turismoInterno" => "Domestic tourism",
+        "ofertaTuristica" => "Tourism supply",
+        "impactoEnElEmpleo" => "Employment impact",
+        "sostenibilidadTuristica" => "Sustainable Tourism"
+    ],
+    "mapa" => "Map"
     
     
     ];
