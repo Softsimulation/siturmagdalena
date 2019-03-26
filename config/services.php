f4a4d95185e06593ea6c4862cf918ddd6edac77c
Jose Montero <josegmp20@gmail.com>
2019-03-26 19:14:37 +0000

    oferta

--- config/services.php
+++ config/services.php
@@ -37,12 +37,12 @@ return [
         'facebook' => [
         'client_id' => '512517055892550',
         'client_secret' => '72db37ae90667c433e6a80b7b4410494',
-        'redirect' => 'https://situr2-luifer.c9users.io/registrar/handleprovidercallback/facebook'
+        'redirect' => request()->getHttpHost().'/registrar/handleprovidercallback/facebook'
     ],
     'google' => [
         'client_id' => '224096646786-023pr43kve1duiehrjmfi2ma17cj48cq.apps.googleusercontent.com',
         'client_secret' => 'eBvULZCNMJ0zAiASmHMwlUoq',
-        'redirect' => 'https://situr2-luifer.c9users.io/registrar/handleprovidercallback/google'
+        'redirect' => 'https://'.request()->getHttpHost().'/registrar/handleprovidercallback/google'
     ]
 
 ];
