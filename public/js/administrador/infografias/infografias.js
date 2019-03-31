3216656fd2114fbd4ca593d953421bac76ecd05d
Jorge Pineda <aplicacionesj@gmail.com>
2019-03-31 23:33:56 +0000

    Se agregó versión inicial de infografia

--- public/js/administrador/infografias/infografias.js
+++ public/js/administrador/infografias/infografias.js
@@ -1,6 +1,6 @@
 var pp=angular.module('admin.infografias', ['infografiasservice'])
 
-.controller('Infografiactrl', ['$scope', 'adminService',function ($scope, adminService) {
+.controller('Infografiactrl', ['$scope', 'adminService', '$timeout',function ($scope, adminService, $timeout) {
 
     $scope.exportaciones=[];
      $("body").attr("class", "cbp-spmenu-push charging");
@@ -18,7 +18,9 @@ var pp=angular.module('admin.infografias', ['infografiasservice'])
     
   
     
-    
+    $scope.getImg = function(url, id){
+        return (id != null && id != undefined) ? url + id + ".png" : '';
+    }
     
     
     
@@ -39,7 +41,15 @@ var pp=angular.module('admin.infografias', ['infografiasservice'])
                 if(data.success){
                     
                    $scope.datoinfografia=data.datos;
-                    
+                   console.log($scope.getItemByName('sexo','Femenino'));
+                   //d3.selectAll(".wrap").call($scope.wrap, 60);
+                   $timeout(function(){
+                       //d3.selectAll(".wrap").call($scope.wrap, 60);
+                       console.log(d3.select('svg').select("#tipo-transporte-1-name").text().split(/\s+/).reverse());
+                       d3.selectAll(".wrap").call($scope.wrap, 70);
+                       //$scope.wrap("#tipo-transporte-1-name", 70);
+                   },1000);
+                   
                 }else{
                     $scope.errores=data.errores;
                     swal("Error","Corrija los errores","success");
@@ -53,7 +63,53 @@ var pp=angular.module('admin.infografias', ['infografiasservice'])
         
     }
     
+    $scope.getItemByName = function(indicator, name){
+        
+        var copyList = Object.assign([],$scope.datoinfografia);
+        var indicatorArr = copyList[indicator];
+        var result = null;
+        if(indicatorArr != undefined){
+            result = indicatorArr.filter(function(item) {
+            	return item.nombre == name;
+            });
+        }
+        return (result != null && result.length > 0) ? result.shift() : {nombre: name, numero: 0};
+    }
     
+    $scope.calcBar = function(percentage){
+        console.log(percentage);
+        return (percentage * 70.8661)/100;
+    }
+    
+    $scope.wrap = function(text, width) {
+      text.each(function() {
+        var text = d3.select(this),
+            textTemp = text.text().indexOf("(") > -1 ? text.text().split('(')[0] : text.text(),
+            words = textTemp.split(/\s+/).reverse(),
+            word,
+            line = [],
+            lineNumber = 0,
+            lineHeight = 1.1, // ems
+            x = text.attr("x"),
+            y = parseFloat(text.attr("y")),
+            //dy = parseFloat(text.attr("dy")),
+            dy = parseFloat(y),
+            tspan = text.text(null).append("tspan").attr("x", x).attr("y", y);
+            lineNumber+=10;
+        while (word = words.pop()) {
+          line.push(word);
+          tspan.text(line.join(" "));
+          if (tspan.node().getComputedTextLength() > width) {
+            line.pop();
+            tspan.text(line.join(" "));
+            line = [word];
+            tspan = text.append("tspan").attr("x", x).attr("y", y + lineNumber).text(word);
+            lineNumber+=10;
+            //tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+          }
+        }
+      });
+    }
     
 
 }])
